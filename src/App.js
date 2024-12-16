8951e82d0e666e18ef19f6be34b72f2b70bfa7ca
FarhanAli04 <farhan.xwave@gmail.com>
2024-12-16 14:29:17 +0500

    navbar created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import HeaderSection from './components/HeaderSection/HeaderSection';
+
+function App() {
+  return (
+    <div className="App">
+        <HeaderSection />
+    </div>
+  );
+}
+
+export default App;
