62516b4319ca9716179868af7d35b0750fbf42ff
FarhanAli04 <farhan.xwave@gmail.com>
2024-12-17 10:31:15 +0500

    carsoul added in hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import HeaderSection from './components/HeaderSection/HeaderSection';
+import { HeroSection } from './components/HeroSection/HeroSection';
 
 function App() {
   return (
-    <div className="App">
-        <HeaderSection />
+    <div>
+      <HeaderSection />
+      <HeroSection />
     </div>
   );
 }
